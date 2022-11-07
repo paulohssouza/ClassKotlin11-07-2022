dd9b4512c3e1aa1cd232729beea0f4463a1a0c04
Paulo Henrique <34384560+paulohssouza@users.noreply.github.com>
2022-11-07 19:30:37 -0300

    Class Kotlin 11-11-2022

---
+++ app/src/main/java/com/cursoandroid/ph/classkotlin11_07_2022/HelloWorld.kt
@@ -0,0 +1,8 @@
+package com.cursoandroid.ph.classkotlin11_07_2022
+
+fun main () {
+    println("Hello World!")
+    println("Digite seu nome:")
+    val name = readln()
+    println("Hello $name!")
+}
