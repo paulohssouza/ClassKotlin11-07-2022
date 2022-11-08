8fcd6d9c07170964af4174553fbdde1870f72a92
Paulo Henrique <34384560+paulohssouza@users.noreply.github.com>
2022-11-07 21:23:01 -0300

    Class Kotlin 11-11-2022 - operators

---
+++ app/src/main/java/com/cursoandroid/ph/classkotlin11_07_2022/Operators.kt
@@ -0,0 +1,16 @@
+package com.cursoandroid.ph.classkotlin11_07_2022
+
+fun main () {
+    //sum
+    println("Soma: 1 + 1")
+    println(1 + 1)
+    //subtraction
+    println("Subtração: 1 - 1")
+    println(1 - 1)
+    //multiplication
+    println("Multiplicação: 1 * 1")
+    println(1 * 1)
+    //divide
+    println("Divisão: 10 / 2")
+    println(10 / 2)
+}
